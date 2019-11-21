20e50aebb3544fe3823ff94a707025a8b6fa2af8
Taras <taraslut78@g.mail.com>
2019-11-21 20:37:38 +0200

    data for home work Speller

--- 13_request_html/checker.py
+++ 13_request_html/checker.py
@@ -2,13 +2,29 @@ import sys
 
 args = sys.argv
 
-print(args)
-
 if len(args) == 3:
     dict_file_name = args[1]
     text_file_name = args[2]
 else:
-    print("Ви погані")
+    print("""Script usage is:
+python.exe checker.py dict_file_name text_file_name
+
+where: 
+    dict_file_name -- dictionary file mane  
+    text_file_name -- file name for spell analysis  
+     """)
+    sys.exit(1)
 
 print(dict_file_name)
 print(text_file_name)
+
+text_dict = ""
+with open(dict_file_name, "r") as file_dict:
+    text_dict = file_dict.read()
+
+text_for_check = ""
+with open(text_file_name, 'r') as file_text:
+    text_for_check = file_text.read()
+
+print(len(text_dict))
+print(len(text_for_check))
---
+++ 13_request_html/demo_sys.py
@@ -0,0 +1,11 @@
+import sys
+from pprint import pprint
+
+# print(sys.path)
+# for i in sys.path:
+#     print(i)
+pprint(sys.path)
+
+dd = {12:sys.path, 1234:'asda', 'sdfsdfsd':13}
+
+pprint(dd)
